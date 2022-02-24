5ff7d17a3d79bf1266ad83ee6b427a6ec3d74bb1
mahdij <mahdimn2011@yahoo.com>
2022-02-24 12:14:16 +0330

    zoom added

--- components/CandleChart.vue
+++ components/CandleChart.vue
@@ -12,6 +12,20 @@
 					width="20"
 				/>
 			</button>
+			<button
+				class="p-1 px-2 text-sm rounded bg-option -my-1"
+				@click="zoomIn"
+				v-if="data.candleData"
+			>
+				+
+			</button>
+			<button
+				class="p-1 px-2 text-sm rounded bg-option -my-1"
+				@click="zoomOut"
+				v-if="data.candleData"
+			>
+				-
+			</button>
 		</div>
 		<div id="stock-container" style="height: 800px; width: 100%"></div>
 		<div v-if="data.candleData" class="ml-2">
@@ -107,6 +121,7 @@ const convertToMinute = (d: string, n: number) => {
 
 //timestamp minute = 6000
 const MINUTE = 60000
+const ZOOM = 3 * MINUTE
 
 export default Vue.extend({
 	data() {
@@ -178,309 +193,337 @@ export default Vue.extend({
 				.filter((it) => it.buy)
 				.map((it) => [Number(it.timestamp), it.price])
 
-			this.chart = HighStock.stockChart('stock-container', {
-				chart: {
-					panning: {
-						enabled: true,
-						type: 'x',
-					},
-				},
-				navigation: {
-					bindingsClassName: 'tools-container',
-				},
-				// time: {
-				// 	useUTC: false,
-				// 	timezone: 'IR',
-				// },
-				xAxis: {},
-				yAxis: [
-					{
-						labels: {
-							align: 'left',
-						},
-						height: '80%',
-						resize: {
+			this.chart = HighStock.stockChart(
+				'stock-container',
+				{
+					chart: {
+						panning: {
 							enabled: true,
+							type: 'x',
 						},
 					},
-					{
-						labels: {
-							align: 'left',
-						},
-						top: '80%',
-						height: '20%',
-						offset: 0,
+					navigation: {
+						bindingsClassName: 'tools-container',
 					},
-				],
-				stockTools: {
-					gui: {
-						enabled: true,
-						buttons: [
-							'indicators',
-							'separator',
-							'simpleShapes',
-							'lines',
-							'measure',
-							'advanced',
-							'verticalLabels',
-							'flags',
-							'separator',
-							'zoomChange',
-							'fullScreen',
-							'separator',
-							'currentPriceIndicator',
-							'saveChart',
-						],
+					mapNavigation: {
+						enableMouseWheelZoom: true,
 					},
-				},
-				plotOptions: {
-					series: {
-						states: {
-							inactive: {
-								enabled: false,
-							},
-						},
-					},
-				},
-				tooltip: {
-					shape: 'square',
-					headerShape: 'callout',
-					borderWidth: 0,
-					shadow: false,
-					positioner: function (width, height, point) {
-						let chart = this.chart,
-							position
-
-						if (point.isHeader) {
-							position = {
-								x: Math.max(
-									// Left side limit
-									chart.plotLeft,
-									Math.min(
-										point.plotX +
-											chart.plotLeft -
-											width / 2,
-										// Right side limit
-										chart.chartWidth - width - 2
-									)
-								),
-								y: point.plotY,
-							}
-						} else {
-							position = {
-								x: point.series
-									? point.series.chart.plotLeft
-									: 0,
-								y: 0 - chart.plotTop,
-							}
-						}
-
-						return position
-					},
-				},
-				responsive: {
-					rules: [
+					// time: {
+					// 	useUTC: false,
+					// 	timezone: 'IR',
+					// },
+					xAxis: {},
+					yAxis: [
 						{
-							condition: {
-								maxWidth: 800,
+							labels: {
+								align: 'left',
 							},
-							chartOptions: {
-								rangeSelector: {
-									inputEnabled: false,
+							height: '80%',
+							resize: {
+								enabled: true,
+							},
+						},
+						{
+							labels: {
+								align: 'left',
+							},
+							top: '80%',
+							height: '20%',
+							offset: 0,
+						},
+					],
+					stockTools: {
+						gui: {
+							enabled: true,
+							buttons: [
+								'indicators',
+								'separator',
+								'simpleShapes',
+								'lines',
+								'measure',
+								'advanced',
+								'verticalLabels',
+								'flags',
+								'separator',
+								'zoomChange',
+								'fullScreen',
+								'separator',
+								'currentPriceIndicator',
+								'saveChart',
+							],
+						},
+					},
+					plotOptions: {
+						series: {
+							states: {
+								inactive: {
+									enabled: false,
 								},
 							},
 						},
-					],
-				},
-				rangeSelector: {
-					buttons: [
-						{
-							type: 'minute',
-							count: 100,
-							text: '1min',
-							preserveDataGrouping: true,
-							dataGrouping: {
-								forced: true,
-								units: [['minute', [1]]],
-							},
-						},
-						{
-							type: 'minute',
-							count: 200,
-							text: '5min',
-							preserveDataGrouping: true,
-							dataGrouping: {
-								forced: true,
-								units: [['minute', [5]]],
-							},
-						},
-						{
-							type: 'minute',
-							count: 400,
-							text: '15min',
-							preserveDataGrouping: true,
-							dataGrouping: {
-								forced: true,
-								units: [['minute', [15]]],
-							},
-						},
-						{
-							type: 'minute',
-							count: 1000,
-							text: '30min',
-							preserveDataGrouping: true,
-							dataGrouping: {
-								forced: true,
-								units: [['minute', [30]]],
-							},
-						},
-						{
-							type: 'minute',
-							count: 500,
-							text: '45min',
-							preserveDataGrouping: true,
-							dataGrouping: {
-								forced: true,
-								units: [['minute', [45]]],
-							},
-						},
-						{
-							type: 'hour',
-							count: 50,
-							text: '1h',
-							preserveDataGrouping: true,
-							dataGrouping: {
-								forced: true,
-								units: [['hour', [1]]],
-							},
-						},
-						{
-							type: 'hour',
-							count: 50,
-							text: '2h',
-							preserveDataGrouping: true,
-							dataGrouping: {
-								forced: true,
-								units: [['hour', [2]]],
-							},
-						},
-						{
-							type: 'hour',
-							count: 50,
-							text: '3h',
-							preserveDataGrouping: true,
-							dataGrouping: {
-								forced: true,
-								units: [['hour', [3]]],
-							},
-						},
-						{
-							type: 'hour',
-							count: 50,
-							text: '4h',
-							preserveDataGrouping: true,
-							dataGrouping: {
-								forced: true,
-								units: [['hour', [4]]],
-							},
-						},
-						{
-							type: 'day',
-							// count: 10,
-							text: '1d',
-							preserveDataGrouping: true,
-							dataGrouping: {
-								forced: true,
-								units: [['day', [1]]],
-							},
-						},
-						{
-							type: 'week',
-							// count: 10,
-							text: '1w',
-							preserveDataGrouping: true,
-							dataGrouping: {
-								forced: true,
-								units: [['week', [1]]],
-							},
-						},
-						{
-							type: 'month',
-							// count: 10,
-							text: '1m',
-							preserveDataGrouping: true,
-							dataGrouping: {
-								forced: true,
-								units: [['month', [1]]],
-							},
-						},
-						{
-							type: 'all',
-							text: 'All',
-						},
-					],
-					allButtonsEnabled: true,
-					selected: 1,
-				},
-				navigator: {
-					enabled: true,
-				},
-				credits: {
-					enabled: false,
-				},
-				series: [
-					{
-						type: 'hollowcandlestick',
-						name: 'Candlestick',
-						id: 'candlestick',
-						data,
 					},
-					{
-						name: 'sell trade',
-						id: 'sell-trade',
-						data: sellData,
-						lineWidth: 0,
-						color: '#8B0000',
-						marker: {
-							enabled: true,
-							fillColor: '#8B0000',
-							symbol: `url(${leftTri})`,
-							marginRight: 4,
-							width: 20,
-							height: 20,
-						},
-						states: {
-							hover: {
-								lineWidthPlus: 0,
-							},
+					tooltip: {
+						shape: 'square',
+						headerShape: 'callout',
+						borderWidth: 0,
+						shadow: false,
+						positioner: function (width, height, point) {
+							let chart = this.chart,
+								position
+
+							if (point.isHeader) {
+								position = {
+									x: Math.max(
+										// Left side limit
+										chart.plotLeft,
+										Math.min(
+											point.plotX +
+												chart.plotLeft -
+												width / 2,
+											// Right side limit
+											chart.chartWidth - width - 2
+										)
+									),
+									y: point.plotY,
+								}
+							} else {
+								position = {
+									x: point.series
+										? point.series.chart.plotLeft
+										: 0,
+									y: 0 - chart.plotTop,
+								}
+							}
+
+							return position
 						},
 					},
-					{
-						name: 'buy trade',
-						id: 'buy-trade',
-						data: buyData,
-						lineWidth: 0,
-						color: 'green',
-						marker: {
-							enabled: true,
-							fillColor: 'green',
+					responsive: {
+						rules: [
+							{
+								condition: {
+									maxWidth: 800,
+								},
+								chartOptions: {
+									rangeSelector: {
+										inputEnabled: false,
+									},
+								},
+							},
+						],
+					},
+					rangeSelector: {
+						buttons: [
+							{
+								type: 'minute',
+								count: 100,
+								text: '1min',
+								preserveDataGrouping: true,
+								dataGrouping: {
+									forced: true,
+									units: [['minute', [1]]],
+								},
+							},
+							{
+								type: 'minute',
+								count: 200,
+								text: '5min',
+								preserveDataGrouping: true,
+								dataGrouping: {
+									forced: true,
+									units: [['minute', [5]]],
+								},
+							},
+							{
+								type: 'minute',
+								count: 400,
+								text: '15min',
+								preserveDataGrouping: true,
+								dataGrouping: {
+									forced: true,
+									units: [['minute', [15]]],
+								},
+							},
+							{
+								type: 'minute',
+								count: 1000,
+								text: '30min',
+								preserveDataGrouping: true,
+								dataGrouping: {
+									forced: true,
+									units: [['minute', [30]]],
+								},
+							},
+							{
+								type: 'minute',
+								count: 500,
+								text: '45min',
+								preserveDataGrouping: true,
+								dataGrouping: {
+									forced: true,
+									units: [['minute', [45]]],
+								},
+							},
+							{
+								type: 'hour',
+								count: 50,
+								text: '1h',
+								preserveDataGrouping: true,
+								dataGrouping: {
+									forced: true,
+									units: [['hour', [1]]],
+								},
+							},
+							{
+								type: 'hour',
+								count: 50,
+								text: '2h',
+								preserveDataGrouping: true,
+								dataGrouping: {
+									forced: true,
+									units: [['hour', [2]]],
+								},
+							},
+							{
+								type: 'hour',
+								count: 50,
+								text: '3h',
+								preserveDataGrouping: true,
+								dataGrouping: {
+									forced: true,
+									units: [['hour', [3]]],
+								},
+							},
+							{
+								type: 'hour',
+								count: 50,
+								text: '4h',
+								preserveDataGrouping: true,
+								dataGrouping: {
+									forced: true,
+									units: [['hour', [4]]],
+								},
+							},
+							{
+								type: 'day',
+								// count: 10,
+								text: '1d',
+								preserveDataGrouping: true,
+								dataGrouping: {
+									forced: true,
+									units: [['day', [1]]],
+								},
+							},
+							{
+								type: 'week',
+								// count: 10,
+								text: '1w',
+								preserveDataGrouping: true,
+								dataGrouping: {
+									forced: true,
+									units: [['week', [1]]],
+								},
+							},
+							{
+								type: 'month',
+								// count: 10,
+								text: '1m',
+								preserveDataGrouping: true,
+								dataGrouping: {
+									forced: true,
+									units: [['month', [1]]],
+								},
+							},
+							{
+								type: 'all',
+								text: 'All',
+							},
+						],
+						allButtonsEnabled: true,
+						selected: 1,
+					},
+					navigator: {
+						enabled: true,
+					},
+					credits: {
+						enabled: false,
+					},
+					series: [
+						{
+							type: 'hollowcandlestick',
+							name: 'Candlestick',
+							id: 'candlestick',
+							data,
+						},
+						{
+							name: 'sell trade',
+							id: 'sell-trade',
+							data: sellData,
+							lineWidth: 0,
+							color: '#8B0000',
+							marker: {
+								enabled: true,
+								fillColor: '#8B0000',
+								symbol: `url(${leftTri})`,
+								marginRight: 4,
+								width: 20,
+								height: 20,
+							},
+							states: {
+								hover: {
+									lineWidthPlus: 0,
+								},
+							},
+						},
+						{
+							name: 'buy trade',
+							id: 'buy-trade',
+							data: buyData,
+							lineWidth: 0,
 							color: 'green',
-							symbol: `url(${rightTri})`,
-							width: 20,
-							height: 20,
-						},
-						states: {
-							hover: {
-								lineWidthPlus: 0,
+							marker: {
+								enabled: true,
+								fillColor: 'green',
+								color: 'green',
+								symbol: `url(${rightTri})`,
+								width: 20,
+								height: 20,
+							},
+							states: {
+								hover: {
+									lineWidthPlus: 0,
+								},
 							},
 						},
-					},
-				] as any[],
-			})
+					] as any[],
+				},
+				(chart) => {
+					HighStock.addEvent(
+						chart.container,
+						'wheel',
+						(event: any) => {
+							if (event.deltaY > 0) this.zoomOut()
+							else if (event.deltaY < 0) this.zoomIn()
+
+							event.preventDefault && event.preventDefault()
+						}
+					)
+				}
+			)
 			this.chart.xAxis[0].setExtremes(
 				data[1][0],
 				data[Math.floor(data.length / 110) + 1][0]
 			)
+		},
+		zoomOut() {
+			const min = this.chart.xAxis[0].getExtremes().min,
+				max = this.chart.xAxis[0].getExtremes().max
+			this.chart.xAxis[0].setExtremes(min - ZOOM, max + ZOOM)
+		},
+		zoomIn() {
+			const min = this.chart.xAxis[0].getExtremes().min,
+				max = this.chart.xAxis[0].getExtremes().max
+			this.chart.xAxis[0].setExtremes(min + ZOOM, max - ZOOM)
 		},
 		disableZoom() {
 			if (this.chart.options.chart) {
@@ -541,9 +584,12 @@ export default Vue.extend({
 					t.dataGrouping && t.dataGrouping.units
 						? t.dataGrouping.units
 						: [['minute', [1]]]
-				
+
 				const n = units[0][1] ? units[0][1][0] : 1
-				const minute = convertToMinute(units[0][0] as string, n as number)
+				const minute = convertToMinute(
+					units[0][0] as string,
+					n as number
+				)
 				m = this.candleNumber * minute
 			}
 			// const r = 10000000 * 5
@@ -554,7 +600,7 @@ export default Vue.extend({
 
 			this.chart.xAxis[0].setExtremes(
 				this.clickedTimestamp - Math.floor(m / 2) * MINUTE,
-				this.clickedTimestamp + Math.floor(m / 2) * MINUTE,
+				this.clickedTimestamp + Math.floor(m / 2) * MINUTE
 			)
 
 			if (this.chart.get('flag')) this.chart.get('flag')?.remove()
